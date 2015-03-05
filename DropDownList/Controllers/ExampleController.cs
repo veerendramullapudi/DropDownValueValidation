a973a5dada39d79bfad02cddaf672b3182072166
veerendramullapudi <veerendramullapudi@gmail.com>
2015-03-05 19:55:38 +0530

    DropDown value Change Validation
    This is done in jquery.

---
+++ DropDownList/Controllers/ExampleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownList.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public  ActionResult   Index()
+        {
+            return View();
+        }
+        public JsonResult check(FormCollection form)
+        {
+            System.Threading.Thread.Sleep(3000);
+            string name = form["username"];
+
+            if (name.Equals("mark"))
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
+        }
+    }
+}
